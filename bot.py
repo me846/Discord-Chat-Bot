6c05c2b0174675c68fae94e515482cc4936142e4
LvaMai <102939446+mid0w0dim@users.noreply.github.com>
2024-10-15 22:14:26 +0900

    update

--- bot.py
+++ bot.py
@@ -2,17 +2,14 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
-from flask import Flask
-from threading import Thread
+from server import server_thread
 
-# Discord bot setup
 load_dotenv()
 
 bot = commands.Bot(command_prefix="!!?", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    # Cogsを読み込む
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
             await bot.load_extension(f'cogs.{filename[:-3]}')
@@ -40,20 +37,6 @@ async def reload(ctx, extension):
     await bot.reload_extension(f'cogs.{extension}')
     await ctx.send(f'Re - Loaded {extension} done.')
 
-# Flask app for health check
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is running"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
-
 if __name__ == '__main__':
-    keep_alive()  # Start the health check server
+    server_thread()
     bot.run(os.getenv('BOT_TOKEN'))
